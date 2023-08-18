740ae76dd690b86e491ecba546bc2f5e6718f0fe
Lucas Ruiz <deth_lucas@hotmail.com>
2023-08-18 11:29:45 -0300

    catch error

--- src/routes/ClientHome/ProductDetails/index.tsx
+++ src/routes/ClientHome/ProductDetails/index.tsx
@@ -3,22 +3,28 @@ import ButtonInverse from "../../../components/ButtonInverse";
 import ButtonPrimary from "../../../components/ButtonPrimary";
 import ProductDetailsCard from "../../../components/ProductDetailsCard";
 import * as productService from "../../../services/product-service";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { Link } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { ProductDTO } from "../../../models/product";
-
 
 export default function ProductDetails() {
   const params = useParams();
 
   const [product, setProduct] = useState<ProductDTO>();
 
+  const navigate = useNavigate();
+
   useEffect(() => {
-    productService.findById(Number(params.productId)).then((response) => {
-      console.log(response.data);
-      setProduct(response.data);
-    });
+    productService
+      .findById(Number(params.productId))
+      .then((response) => {
+        console.log(response.data);
+        setProduct(response.data);
+      })
+      .catch(() => {
+        navigate("/");
+      });
   }, []);
 
   return (
