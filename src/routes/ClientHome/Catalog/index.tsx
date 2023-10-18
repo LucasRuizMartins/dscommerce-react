f37c220918a999f40a8cea8a2d2d8b76248f11cb
Lucas Ruiz <deth_lucas@hotmail.com>
2023-10-18 09:50:10 -0300

    Query Params

--- src/routes/ClientHome/Catalog/index.tsx
+++ src/routes/ClientHome/Catalog/index.tsx
@@ -7,34 +7,38 @@ import CatalogCard from "../../../components/CatalogCard";
 import * as productService from "../../../services/product-service";
 import { useEffect, useState } from "react";
 import { ProductDTO } from "../../../models/product";
-import { CategoryDTO } from "../../../models/category";
-import { json } from "react-router-dom";
 
+type QueryParams = {
+  page: number;
+  name: string;
+};
 
 export default function Catalog() {
   const [products, setProducts] = useState<ProductDTO[]>([]);
 
-  const objTest : CategoryDTO = {
-  id : 8,
-  name : "Jardinagem"
-  }
+  const [queryParams, setQueryParams] = useState<QueryParams>({
+    page:0,
+    name:""
+  });
 
   useEffect(() => {
-
     //localStorage.setItem("minhaCategoria", JSON.stringify(objTest))
-    const obj = JSON.parse(localStorage.getItem("minhaCategoria") || "{}"); 
-    console.log(obj.name)
+    const obj = JSON.parse(localStorage.getItem("minhaCategoria") || "{}");
+    console.log(obj.name);
 
-
-    productService.findAll().then((response) => {
+    productService.findPageRequest(queryParams.page, queryParams.name).then((response) => {
       setProducts(response.data.content);
     });
-  }, []);
+  }, [queryParams]);
+
+  function handleSearch(searchText: string) {
+    setQueryParams({...queryParams, name:searchText});
+  } //
 
   return (
     <main>
       <section id="catalog-section" className="dsc-container">
-        <SearchBar />
+        <SearchBar onSearch={handleSearch} />
 
         <div className="dsc-catalog-cards dsc-mb20 dsc-mt20">
           {products.map((product) => (
@@ -42,7 +46,7 @@ export default function Catalog() {
           ))}
         </div>
 
-        <ButtonNextPage nameButton="TESTE" />
+        <ButtonNextPage nameButton="VER MAIS" />
       </section>
     </main>
   );
